b319efb742ce634770f5550a8d83f827c2bfebc2
koirand <koirand.jp@gmail.com>
2019-05-01 01:16:33 +0900

    🐸 Refactoring

--- main.go
+++ main.go
@@ -1,29 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"strings"
 
+	"github.com/chromedp/chromedp"
 	"github.com/koirand/slack-balance/config"
 	"github.com/koirand/slack-balance/slack"
 	"github.com/koirand/slack-balance/ufj"
 	"github.com/koirand/slack-balance/utils"
 )
 
-const jsonBody string = `{
+const jsonStr string = `{
 	"text": ":moneybag: 三菱UFJ銀行の残高は %s です！",
 }
 `
 
 func main() {
 	utils.LoggingSetting()
-	balance, err := ufj.GetBalance(config.Config.UfjId, config.Config.UfjPassword)
+
+	// create chrome instance
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	// run task list
+	var balance string
+	err := chromedp.Run(ctx, ufj.GetBalance(config.Config.UfjId, config.Config.UfjPassword, &balance))
 	if err != nil {
 		log.Fatalf("Failed to get balance: %s\n", err.Error())
 	}
-	message := fmt.Sprintf(jsonBody, balance)
+	log.Printf("UFJ bank balance: %s", balance)
 
+	// send to slack
+	message := fmt.Sprintf(jsonStr, balance)
 	if err := slack.SendMessage(config.Config.WebhookUrl, strings.NewReader(message)); err != nil {
 		log.Fatalf("Failed to send to slack: %s\n", err.Error())
 	}
--- ufj/ufj.go
+++ ufj/ufj.go
@@ -1,37 +1,19 @@
 package ufj
 
 import (
-	"context"
-	"log"
-	"time"
-
 	"github.com/chromedp/chromedp"
 )
 
 const url = "https://entry11.bk.mufg.jp/ibg/dfw/APLIN/loginib/login?_TRANID=AA000_001"
 
-func GetBalance(id string, password string) (balance string, err error) {
-	// create context
-	ctx, cancel := chromedp.NewContext(context.Background(), chromedp.WithDebugf(log.Printf))
-	defer cancel()
-
-	// create a timeout
-	ctx, cancel = context.WithTimeout(ctx, 10*time.Second)
-	defer cancel()
-
-	// run task list
-	err = chromedp.Run(ctx,
+func GetBalance(id string, password string, balance *string) chromedp.Tasks {
+	return chromedp.Tasks{
 		chromedp.Navigate(url),
-		chromedp.WaitVisible("#footer"),
+		chromedp.WaitVisible("#footer", chromedp.ByID),
 		chromedp.SendKeys(`//input[@name='KEIYAKU_NO']`, id),
 		chromedp.SendKeys(`//input[@name='PASSWORD']`, password),
 		chromedp.Click(`//*[@id="login_frame"]/div/div/div[2]/a/img`),
-		chromedp.WaitVisible("#footer"),
-		chromedp.Text(`#setAmountDisplay`, &balance),
-	)
-	if err != nil {
-		return "", err
+		chromedp.WaitVisible("#footer", chromedp.ByID),
+		chromedp.Text(`#setAmountDisplay`, balance, chromedp.ByID),
 	}
-
-	return balance, nil
 }
